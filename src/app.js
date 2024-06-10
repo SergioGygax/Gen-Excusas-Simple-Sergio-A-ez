f3c287ef3a5ad8c3c069f4318e3b285ef3c6a741
Sergio Añez <160168084+SergioGygax@users.noreply.github.com>
2024-06-10 16:29:43 +0000

    Generador de excusas simple Sergio Añez

--- src/app.js
+++ src/app.js
@@ -5,7 +5,26 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
+
+const generateExcuse = () => {
+  let who = ["Mi perro", "Mi abuela", "El cartero", "Mi gato"];
+  let action = ["se comió", "rompió", "desapareció", "destruyó"];
+  let what = ["mi tarea", "mi móvil", "la casa", "el correo"];
+  let when = [
+    "antes de la clase",
+    "cuando estaba entrenando",
+    "cuando estaba en el trabajo",
+    "mientras dormía"
+  ];
+
+  let whoIdx = Math.floor(Math.random() * who.length);
+  let actionIdx = Math.floor(Math.random() * action.length);
+  let whatIdx = Math.floor(Math.random() * what.length);
+  let whenIdx = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIdx]} ${action[actionIdx]} ${what[whatIdx]} ${when[whenIdx]}`;
 };
